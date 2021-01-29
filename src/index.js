d7c2a262252ea1a06b0ddc674078d41c7354c67d
cmn <cedric.menou@outlook.com>
2021-01-30 00:16:06 +0100

    Allow elment upgrades

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import { PhotoElement } from "./components/photo/photo.element.js";
 import { ZoomElement } from "./components/zoom/zoom.element.js";
 
 // Focus web components
-window.customElements.define("img-focus", FocusElement);
-window.customElements.define("img-photo", PhotoElement);
 window.customElements.define("img-zoom", ZoomElement);
+window.customElements.define("img-photo", PhotoElement);
+window.customElements.define("img-focus", FocusElement);
